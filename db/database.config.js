ea0d4b8b0e8edb7cfd015b4109d27dc6426330bc
Vu_T-Solution <leanhvu86@gmail.com>
2020-06-06 14:46:55 +0700

    update database

--- db/database.config.js
+++ db/database.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/oauth'
+    url: 'mongodb://localhost:27017/booking'
 };
 // module.exports = {
 //     url: 'mongodb://leanhvu:123456aA@ds215988.mlab.com:15988/heroku_4b8v5f4t'
